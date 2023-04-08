e8b81fac50716dade052ccf4175a6a8d1ea42925
Rushan <ektoeror777@gmail.com>
2023-04-08 12:55:49 +0300

    Seminar_3

--- Seminar_3/Task_1.java
+++ Seminar_3/Task_1.java
@@ -14,17 +14,17 @@ public class Task_1 {
 
         for (int i = 0; i < list.size() - 1; i++) {
             int minIndex = i;
-            for (int j = 0; j < list.size(); j++) {
+            for (int j = i; j < list.size(); j++) {
                 if (list.get(minIndex)>list.get(j)) {
                     minIndex = j;
                 }
                 if (minIndex != i) {
-                    int temp = list.get(i);
-                    list.get(i) = list.get(minIndex); //выдает ошибку не совсем понимаю,что не так
-                    list.get(minIndex) = temp;
+                    int temp = list.get(minIndex);
+                    list.set(minIndex, list.get(i));
+                    list.set(i, temp);
                 }
             }
         }
+        System.out.println(list);
     }
 }
-//сортровку слиянием изучаю в данный момент,реализовать не успеваю
